d207d9f045d1f1fb16423030c6d46a0211f81e5c
Dmitry Ivanov <diemon_2000@mail.ru>
2023-08-31 15:26:27 +0300

    ДизайнДобавлен таймер и рандомное определение цифр

--- SWSU_Randomizer/MainWindow.xaml.cs
+++ SWSU_Randomizer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace SWSU_Randomizer
 {
@@ -24,6 +30,7 @@ namespace SWSU_Randomizer
         {
             InitializeComponent();
         }
+        DispatcherTimer timer = new DispatcherTimer();
 
         private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
@@ -46,5 +53,116 @@ namespace SWSU_Randomizer
         {
 
         }
+
+        private void textBox1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void textBox2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+
+        //Таймер
+        
+
+        private void Start_button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Start_button.Content.ToString();
+            switch (name)
+            {
+                case "Старт":
+                    if (textBox1.Text == "" && textBox1.Text == null)
+                    {
+                        MessageBox.Show("Заполните значение в первом поле");
+                    }
+                    else if (textBox2.Text == "" && textBox2.Text == null)
+                    {
+                        MessageBox.Show("Заполните значение во втором поле");
+                    }
+                    else if (int.Parse(textBox1.Text) > int.Parse(textBox2.Text))
+                    {
+                        MessageBox.Show("Значение первого поля должно быть меньше второго");
+                    }
+                    else
+                    {
+                        //Таймер на 7 секунд
+                        var timerstate = new TimerState { Counter = 0 };
+
+                        var counter = 0;
+
+                        //меняем текст кнопки
+                        Start_button.Content = "Стоп";
+
+                        
+                      
+                            //Начинаем изменять значение
+                            timer.Tick += new EventHandler(timer_Tick);
+                            timer.Interval = TimeSpan.FromSeconds(0.1);
+
+                            Random rnd = new Random();
+                            timer.Start();
+                           
+                        
+                        
+
+                          
+
+                        
+                        //Random rnd = new Random();
+                        //int value = rnd.Next(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                        //TextBlockRandom.Text=($"{value}");
+
+                    }
+                    break;
+                case "Стоп":
+                    {
+                       
+                        Start_button.Content= "Старт";
+                        timer.Stop();
+                        //MessageBox.Show($"Выбрано число: {TextBlockRandom.Text} ");
+                    }
+                    break;
+            }
+           
+            
+             
+           
+
+        }
+        Random rnd = new Random();
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+                Random rnd = new Random();
+                int value = rnd.Next(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                TextBlockRandom.Text=($"{value}");
+            
+        }
+
+
+        public void Roll(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            TextBlockRandom.Text=($"{value}");
+        }
+            //int increment = 1;
+            //increment++;
+            //if(increment >= 250)
+            //{
+            //    timer.Stop();
+            //    increment = 0;
+            //}
+            //TextBlockRandom.Text = $"{rnd.Next(int.Parse(textBox1.Text), int.Parse(textBox2.Text))}";
+
+            class TimerState
+        {
+            public int Counter;
+        }
+
     }
-}
+    }
+
